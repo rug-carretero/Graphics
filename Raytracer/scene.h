5f3688a70ead6fbdb5cad2741dbcc55a7cc406e3
Blue-Motion <a.offereins+github@student.rug.nl>
2015-03-04 14:57:16 +0100

    Revert to 56e05fced

---
+++ Raytracer/scene.h
@@ -0,0 +1,66 @@
+//
+//  Framework for a raytracer
+//  File: scene.h
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#ifndef SCENE_H_KNBLQLP6
+#define SCENE_H_KNBLQLP6
+
+#include <vector>
+#include "triple.h"
+#include "light.h"
+#include "object.h"
+#include "image.h"
+
+class Scene
+{
+private:
+    std::vector<Object*> objects;
+    std::vector<Light*> lights;
+	
+	Color phongReflect(const Ray &ray, int level, Object * src);
+    void phongRender(Image &img);
+    void normalRender(Image &img);
+    void zRender(Image &img);
+    Color phongTrace(const Ray &ray, int level);
+	Color goochTrace(const Ray &ray);
+	Hit trace(const Ray& ray, Object ** obj);
+	
+    double zmin;
+    double zmax;
+public:
+    void render(Image &img);
+    void addObject(Object *o);
+    void addLight(Light *l);
+    void setEye(Triple e);
+    unsigned int getNumObjects() { return objects.size(); }
+    unsigned int getNumLights() { return lights.size(); }
+	
+	enum RenderModes {RenderPhong = 1, RenderZBuffer, RenderNormal, RenderGooch} renderMode;
+	bool renderShadows;
+	int superSamples;
+	int reflectRecursion;
+	
+	double alpha;
+	double beta;
+	double goochB;
+	double goochY;
+	
+    Triple eye;
+    Triple center;
+    Vector up;
+    int width, height;
+};
+
+#endif /* end of include guard: SCENE_H_KNBLQLP6 */
---
+++ Raytracer/sphere.cpp
@@ -0,0 +1,106 @@
+//
+//  Framework for a raytracer
+//  File: sphere.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include "sphere.h"
+#include "image.h"
+#include "material.h"
+#include <iostream>
+#include <math.h>
+
+/************************** Sphere **********************************/
+
+Hit Sphere::intersect(const Ray &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    * 
+    * Insert calculation of ray/sphere intersection here. 
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+	/*
+		zie ook
+		http://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+	*/
+
+	Vector distance = ray.O - position;
+	Vector direction = ray.D.normalized();
+	
+	double a = direction.dot(direction);
+	double b = 2 * (direction.dot(distance));
+	double c = distance.dot(distance) - r * r;
+	double discriminant = b * b - 4 * a * c;
+	
+	if(discriminant < 0) return Hit::NO_HIT();
+	
+	double intersect1 = (-b + sqrt(discriminant)) / (2 * a);
+	double intersect2 = (-b - sqrt(discriminant)) / (2 * a);
+	
+	if(intersect1 < 0 && intersect2 < 0){
+		return Hit::NO_HIT();
+	}
+	
+	double t = 0;
+	
+	if(intersect1 > 0 && intersect2 > 0){
+		t = min(intersect1, intersect2);
+	}else{
+		t = max(intersect1, intersect2);
+	}
+	
+	/****************************************************
+	* RT1.2: NORMAL CALCULATION
+	*
+	* Given: t, C, r
+	* Sought: N
+	* 
+	* Insert calculation of the sphere's normal at the intersection point.
+	****************************************************/
+	
+	Vector N = (ray.O + direction * t - position).normalized();
+	// if(distance.length() < r) N *= -1;
+	if(N.dot(-ray.D) < 0) N = -N;
+	
+	//Vector N = (ray.O + direction.normalized() * t - position).normalized();
+	
+	return Hit(t,N);
+}
+
+Color Sphere::mapTexture(const Point in){
+	Image * texture = material->texture;
+	
+	if(!texture) return material->color;
+
+	//with variable angle, but distorted
+	Vector vn = Vector(0, 1, 0);//.rotate(angle, axis);
+	Vector ve = Vector(1, 0, 0);//.rotate(angle, axis);
+	Vector vp = (in - position).normalized();
+
+	//working, but with no variable angle
+	Vector d = (position - in).normalized().rotate(angle, axis);	
+	double u = 0.5 + atan2(d.z,d.x) / (2.0 * M_PI);
+	double v = 0.5 - asin(d.y) / M_PI;
+	
+	return texture->colorAt(u, v);
+}
